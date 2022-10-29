924758ea02f6edb8773f13ca920aa7f579a1508c
Igor Farias <igorfarias30@hotmail.com>
2022-10-29 14:51:27 -0300

    WIP

--- dags/helpers/etl.py
+++ dags/helpers/etl.py
@@ -1,2 +1,15 @@
+from typing import Tuple
+import pandas as pd
+
 def my_funcion(x):
     return x +  " hello D.E."
+
+def split_points(point: str) -> Tuple[str]:
+    if point.startswith("POINT"):
+        point = point.split("POINT")[-1]
+        points = point.strip().replace("(", "").replace(")", "").split(" ")
+        return (points[0], points[1])
+
+def process_data(filename: str):
+    data = pd.read_csv(filename)
+    columns = data.columns
